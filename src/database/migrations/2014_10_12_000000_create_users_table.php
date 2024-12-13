131069229af29f046b4d6f1fad70c978b3824576
zx-636 <kamail5645@gmail.com>
2024-12-13 23:01:28 +0900

    シーディング前

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigIncrements('id');
+            $table->string('name', 255)->notNull();
+            $table->string('email', 255)->notNull()->unique();
+            $table->string('password', 255)->notNull();
             $table->timestamps();
         });
     }
---
+++ src/database/migrations/2024_12_13_224455_create_contacts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->tinyInteger('gender');
+            $table->string('email', 255);
+            $table->string('tell', 255);
+            $table->string('address', 255);
+            $table->string('building', 255)->nullable();
+            $table->text('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
